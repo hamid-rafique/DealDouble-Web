a1bb8a47acbf77717d87c037a6cb23d3d996098f
hamid-hawklogix <71008760+hamid-hawklogix@users.noreply.github.com>
2021-03-05 01:19:18 +0500

    Added auction table, class and migrations

---
+++ DealDouble.Data/DealDoubleContext.cs
@@ -0,0 +1,18 @@
+﻿using DealDouble.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealDouble.Data
+{
+    class DealDoubleContext : DbContext
+    {
+        public DealDoubleContext() : base("name= DealDoubleConnectionString")
+        {
+        }
+        public DbSet <Auction> Auctions { get; set; }
+    }
+}
