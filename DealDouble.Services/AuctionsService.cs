c08a92aa1923dc731e39b13dafc309d040c27eee
hamid-hawklogix <71008760+hamid-hawklogix@users.noreply.github.com>
2021-03-05 20:58:52 +0500

    Implemented the CRUD related to auctions
    Includes auctions view screens, controller and service methods.

--- DealDouble.Services/AuctionsService.cs
+++ DealDouble.Services/AuctionsService.cs
@@ -16,5 +16,28 @@ namespace DealDouble.Services
             context.Auctions.Add(auction);
             context.SaveChanges();
         }
+
+        public Auction GetAuctionByID(int ID)
+        {
+            DealDoubleContext context = new DealDoubleContext();
+            return context.Auctions.Find(ID);
+        }
+        public void UpdateAuction(Auction auction)
+        {
+            DealDoubleContext context = new DealDoubleContext();
+            context.Entry(auction).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+        }
+        public List<Auction> GetAllAuctions()
+        {
+            DealDoubleContext context = new DealDoubleContext();
+            return context.Auctions.ToList();
+        }
+        public void DeleteAuction(Auction auction)
+        {
+            DealDoubleContext context = new DealDoubleContext();
+            context.Entry(auction).State = System.Data.Entity.EntityState.Deleted;
+            context.SaveChanges();
+        }
     }
 }
--- DealDouble.Web/Controllers/AuctionsController.cs
+++ DealDouble.Web/Controllers/AuctionsController.cs
@@ -10,6 +10,13 @@ namespace DealDouble.Web.Controllers
 {
     public class AuctionsController : Controller
     {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            AuctionsService auctionsService = new AuctionsService();
+            var auctions = auctionsService.GetAllAuctions();
+            return View(auctions);
+        }
       [HttpGet]
         public ActionResult Create()
         {
@@ -21,6 +28,41 @@ namespace DealDouble.Web.Controllers
             AuctionsService auctionsService = new AuctionsService();
             auctionsService.SaveAuction(auction);
             return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            AuctionsService auctionsService = new AuctionsService();
+            var auction = auctionsService.GetAuctionByID(ID);
+            return View(auction);
+        }
+        [HttpPost]
+        public ActionResult Edit(Auction auction)
+        {
+            AuctionsService auctionsService = new AuctionsService();
+            auctionsService.UpdateAuction(auction);
+            return RedirectToRoute(new { controller = "Auctions", action = "Index" });
+        }
+        [HttpPost]
+        public ActionResult Delete(Auction auction, int option)
+        {
+            if (option == 1)
+            {
+                AuctionsService auctionsService = new AuctionsService();
+                auctionsService.DeleteAuction(auction);
+                return RedirectToAction("Index");
+            }
+            else
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            AuctionsService auctionsService = new AuctionsService();
+            var auction = auctionsService.GetAuctionByID(ID);
+            return View(auction);
+
         }
     }
 }
